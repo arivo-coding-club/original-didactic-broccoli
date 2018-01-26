1a0cc798cdabcaf6e0f27047dd5018f3613a64f1
lee1707 <leegang1707@gmail.com>
2018-01-26 12:54:21 +0900

    주석을 달았습니다

--- src/application/Model.java
+++ src/application/Model.java
@@ -1,9 +1,8 @@
 package application;
 
-//import javafx.scene.control.Button;
-
 public class Model {
 
+	//추가선택을 클릭할 경우 기존의 음료 가격에 새로운 음료 가격이 더해지고, 그렇지 않을 경우 하나의 음료 가격만 계산됩니다
 	public int calculate(String select, int x, int y) {
 		if(!select.equals("추가선택")) {
 			return y;
